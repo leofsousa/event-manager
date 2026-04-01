87dd6bbcea5f6f93145ddcffc94b0f98248df409
leofsousa <leofsousa98@gmail.com>
2026-03-31 22:06:21 -0300

    select button

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -5,9 +5,28 @@ type Option = {
 
 type SelectProps = {
     value: string;
-
+    onChange: (value: string) => void;
+    options: Option[];
+    error?: boolean;
 }
 
-export default function Select() {
-    return
+export default function Select({
+    value,
+    onChange,
+    options,
+    error
+}: SelectProps) {
+    return (
+        <select
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            className={`w-full px-3 py-2 ronded-lg border 
+            bg-white text-gray-900 dark:bg-gray-800 dark:border-gray-700 
+            dark:text-gray-100 focus:outline-none focus:ring-2 
+            focus:ring-blue-500 ${error ? "border-red-500 focus:ring-red-500" : "border-gray-300"}
+            `}
+        >
+            <option value="">Selecione um tipo</option>
+        </select>
+    )
 }
